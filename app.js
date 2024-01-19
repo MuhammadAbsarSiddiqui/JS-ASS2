03bd34e4a546b3a617995dbcb36ba9d1c720fffe
MuhammadAbsarSiddiqui <128141992+MuhammadAbsarSiddiqui@users.noreply.github.com>
2024-01-19 23:44:31 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+//ASSIGNMENT 02 
+
+//Q1
+console.log("****Q1****")
+var length = 25;
+var width = 7.5;
+var area;
+
+area = width * length;
+console.log("the area of rectangle is",area);
+
+
+
+
+let userName = prompt('Enter your name:');
+alert("Hello! " + userName + "Wellcome to our website i have completed my assignment 2 both questions.")
